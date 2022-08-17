86f7aa4aa74fb5b71d314226eb9f69ae9664b9aa
mskim <mskim@11h11m.com>
2022-08-17 11:08:07 +0900

    Initial commit

---
+++ src/main/java/com/demo/musinsashop/MusinsaShopApplication.java
@@ -0,0 +1,13 @@
+package com.demo.musinsashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaShopApplication.class, args);
+    }
+
+}
